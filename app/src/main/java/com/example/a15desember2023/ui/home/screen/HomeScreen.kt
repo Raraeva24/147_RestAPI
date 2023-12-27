8939d014d0770d040d9666f8032665a13f327588
Rara Eva Maharani <rara.eva.ft21@mail.umy.ac.id>
2023-12-27 22:01:58 +0700

    Menambahkan IconButton

--- app/src/main/java/com/example/a15desember2023/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/a15desember2023/ui/home/screen/HomeScreen.kt
@@ -19,11 +19,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -103,17 +105,19 @@ fun KontakCard(
         ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
-
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
                     text = kontak.nama,
                     style = MaterialTheme.typography.titleLarge
                 )
                 Spacer(Modifier.weight(1f))
-                Icon(
-                    imageVector = Icons.Default.Phone,
-                    contentDescription = null
-                )
+                IconButton(onClick = { onDeleteClick(kontak) }) {
+                    Icon(
+                        imageVector = Icons.Default.Phone,
+                        contentDescription = null
+                    )
+                }
                 Text(
                     text = kontak.nohp,
                     style = MaterialTheme.typography.titleMedium
