eb7d50cebdc4ec84947ba02cba3421bed8043877
Rara Eva Maharani <rara.eva.ft21@mail.umy.ac.id>
2023-12-28 23:53:40 +0700

    menambahkan EntryKontakScreen

--- app/src/main/java/com/example/a15desember2023/ui/kontak/screen/InsertScreem.kt
+++ app/src/main/java/com/example/a15desember2023/ui/kontak/screen/InsertScreem.kt
@@ -1,22 +1,36 @@
 package com.example.a15desember2023.ui.kontak.screen
 
+import android.os.Build
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.a15desember2023.navigation.DestinasiNavigasi
+import com.example.a15desember2023.ui.PenyediaViewModel
 import com.example.a15desember2023.ui.kontak.viewmodel.InsertUiEvent
 import com.example.a15desember2023.ui.kontak.viewmodel.InsertUiState
+import com.example.a15desember2023.ui.kontak.viewmodel.InsertViewModel
+import com.example.a15desember2023.ui.theme.TopAppBarKontak
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -80,8 +94,9 @@ fun EntryKontakBody(
     onSiswaValueChange: (InsertUiEvent) -> Unit,
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
-){
-    Column(verticalArrangement = Arrangement.spacedBy(18.dp),
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
         modifier = modifier.padding(12.dp)
     ) {
         FormInputSiswa(
@@ -95,6 +110,53 @@ fun EntryKontakBody(
             modifier = Modifier.fillMaxWidth()
         ) {
             Text(text = "Simpan")
+
+        }
+
+    }
+}
+    object DestinasiEntry : DestinasiNavigasi {
+        override val route: String
+            get() = "item_entry"
+        override val titleRes = "Entry Siswa"
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    @Composable
+    fun EntryKontakScreen(
+        navigateBack: () -> Unit,
+        modifier: Modifier = Modifier,
+        viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+    ) {
+        val coroutineScope = rememberCoroutineScope()
+        val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+        Scaffold(
+            modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                TopAppBarKontak(
+                    title = DestinasiEntry.titleRes,
+                    canNavigateBack = true,
+                    scrollBehavior = scrollBehavior,
+                    navigateUp = navigateBack
+                )
+            }
+        ) { innerPadding ->
+            EntryKontakBody(
+                insertUiState = viewModel.insertKontakState,
+                onSiswaValueChange = viewModel::updateInsertKontakState,
+                onSaveClick = {
+                    coroutineScope.launch {
+                        viewModel.insertKontak()
+                        navigateBack()
+                    }
+                },
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .verticalScroll(rememberScrollState())
+                    .fillMaxWidth()
+            )
 
         }
 
