ef03477c72655bb43f9e36c7b6960f54b6c885bd
Rara Eva Maharani <rara.eva.ft21@mail.umy.ac.id>
2023-12-28 23:46:27 +0700

    membuat kolom nama

--- app/src/main/java/com/example/a15desember2023/ui/kontak/screen/InsertScreem.kt
+++ app/src/main/java/com/example/a15desember2023/ui/kontak/screen/InsertScreem.kt
@@ -2,7 +2,10 @@ package com.example.a15desember2023.ui.kontak.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -21,6 +24,14 @@ fun FormInputSiswa(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ){
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = {onValueChange(insertUiEvent.copy(nama = it))},
+            label = { Text("Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
 
     }
 
