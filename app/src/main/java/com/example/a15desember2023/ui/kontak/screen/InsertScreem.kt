d92bd232a0aaaa42205dd751d52230335136199f
Rara Eva Maharani <rara.eva.ft21@mail.umy.ac.id>
2023-12-28 23:44:50 +0700

    membuat form input siswa pada InsertScreem

--- app/src/main/java/com/example/a15desember2023/ui/kontak/screen/InsertScreem.kt
+++ app/src/main/java/com/example/a15desember2023/ui/kontak/screen/InsertScreem.kt
@@ -1,2 +1,27 @@
 package com.example.a15desember2023.ui.kontak.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.a15desember2023.ui.kontak.viewmodel.InsertUiEvent
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+
+    }
+
+}
