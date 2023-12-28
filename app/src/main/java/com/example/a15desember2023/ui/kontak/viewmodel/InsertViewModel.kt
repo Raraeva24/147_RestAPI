92e292759c36f2419896b35036496a19e7b8e5aa
Rara Eva Maharani <rara.eva.ft21@mail.umy.ac.id>
2023-12-28 23:37:18 +0700

    mengisi class insertKontakState

--- app/src/main/java/com/example/a15desember2023/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/a15desember2023/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,4 +1,57 @@
 package com.example.a15desember2023.ui.kontak.viewmodel
 
-class InsertViewModel {
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a15desember2023.model.Kontak
+import com.example.a15desember2023.repository.KontakRepository
+import kotlinx.coroutines.launch
+
+class InsertViewModel (private val kontakRepository: KontakRepository) : ViewModel(){
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak(){
+        viewModelScope.launch{
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+
+        }
+    }
+
 }
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val email: String = "",
+    val nohp: String = ""
+)
+
+fun InsertUiEvent.toKontak(): Kontak = Kontak(
+    id = id,
+    nama = nama,
+    alamat = email,
+    nohp = nohp
+)
+
+fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    nama = nama,
+    email = alamat,
+    nohp = nohp
+)
